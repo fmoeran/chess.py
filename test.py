5054618feb7e01dfbd43e26a88079df2c8b27cd7
Felix Moeran <felixmoeran@gmail.com>
2023-06-26 12:19:53 +0100

    remove gmpy dependency

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import engine
 
-game = engine.Game(black_is_ai=True)
+game = engine.Game(black_is_ai=True, white_is_ai=True)
 bot = engine.Bot()
 
-game.set_ai(bot.search, 1)
+game.set_ai(bot.search, engine.pieces.black)
+game.set_ai(bot.search, engine.pieces.white)
 
 game.run()
 
 
+print(engine.evaluate.timer)
