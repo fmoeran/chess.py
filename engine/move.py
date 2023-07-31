ca9a3915152f3559fd5fa9137abc48e4b235a7e9
Felix Moeran <felixmoeran@gmail.com>
2023-07-31 14:35:26 +0100

    Refactor and add comments

--- engine/move.py
+++ engine/move.py
@@ -1,10 +1,15 @@
+from engine import pieces
+
 from enum import Enum
 
 column_letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
-piece_letters = ["P", "N", "B", "R", "Q", "K"]
 
 
 class Flags(Enum):
+    """
+    enum for every type of move.
+    e.g.: Flags.en_passent
+    """
     none = 0
     promotion = 1
     en_passent = 2
@@ -14,42 +19,47 @@ class Flags(Enum):
 # a class to hold an instance of a type of move
 class Move:
     def __init__(self, start: int, end: int, flag=Flags.none, promotion_piece=0):
-        '''
-
+        """
         :param start: bitmap of start position
         :param end: bitmap of end position
         :param flag: a flag from Flags(Enum)
         :param promotion_piece: the piece type to promote to if flag = Flags.promotion
-        '''
+        """
         self.start = start
         self.end = end
         self.flag = flag
         self.promotion_piece = promotion_piece
 
     def notate(self):
+        """
+        returns a string of the coordinate notation of the move being represented
+        e.g. e7e8Q (promotion onto e8 into queen)
+        """
         position = 0
         start_pos = 0
         end_pos = 0
-        while 1 << position < 1 << 63:
+        # iterate all positions to find the int positions
+        # of the start and end points
+        while (1 << position) <= (1 << 63):
             if self.start == 1 << position:
                 start_pos = position
             if self.end == 1 << position:
                 end_pos = position
             position += 1
+        #                            column                    row
         starting = column_letters[7 - start_pos % 8] + str(start_pos // 8 + 1)
-        ending = column_letters[7- end_pos % 8] + str(end_pos // 8 + 1)
+        ending = column_letters[7 - end_pos % 8] + str(end_pos // 8 + 1)
         promotion = ""
+        # if the move is a promotion add that piece's character onto the end
         if self.flag == Flags.promotion:
-            promotion = str(piece_letters[self.promotion_piece])
+            promotion = str(pieces.letters[self.promotion_piece])
         return starting + ending + promotion
 
     def __repr__(self):
         return self.notate()
 
     def __eq__(self, other):
-        if type(other) != type(self): return False
-        return self.start == other.start and self.end == other.end and self.promotion_piece == other.promotion_piece \
-            and self.flag == other.flag
+        if type(other) != type(self):
+            return False
+        return self.start == other.start and self.end == other.end and self.promotion_piece == other.promotion_piece and self.flag == other.flag
 
-    def __hash__(self):
-        return self.start + 64 * self.end
--- engine/pieces.py
+++ engine/pieces.py
@@ -1,10 +1,10 @@
-letters = ["P", "N", "B", "R", "Q", "K"]
+letters = "PNBRQK"
 values = {piece_letter: value for value, piece_letter in enumerate(letters)}
 
 coloured_letters = "PNBRQKpnbrqk"
 coloured_values = {piece_letter: value for value, piece_letter in enumerate(coloured_letters)}
 
-
+# piece values
 pawn = 0
 knight = 1
 bishop = 2
@@ -12,23 +12,29 @@ rook = 3
 queen = 4
 king = 5
 
+# colour values
 white = 0
 black = 1
 
 
+# A class to encapsulate a piece value and colour
 class Piece:
     def __init__(self, type, colour):
         self.type = type
         self.colour = colour
 
     def __repr__(self):
-        letter = letters[self.type]
-        if self.colour == black:
-            letter = letter.lower()
+        """
+        returns the character for the represented piece type. e.g. K for white king
+        """
+        letter = coloured_letters[hash(self)]
         return letter
 
     def __hash__(self):
-        return self.type + self.colour*6
+        """
+        returns the same value as coloured_values[str(self)]
+        """
+        return self.type + self.colour * 6
 
     def __eq__(self, other):
         if type(other) != type(self):
