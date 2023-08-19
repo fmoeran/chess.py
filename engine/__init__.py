62139d3380a947db03802f514db073c3deede459
Felix Moeran <felixmoeran@gmail.com>
2023-08-19 13:37:54 +0100

    Add evaluations to init

--- engine/__init__.py
+++ engine/__init__.py
@@ -3,9 +3,10 @@ import os
 cd = os.path.dirname(os.path.abspath(__file__))
 os.chdir(cd)
 
-
 from engine import game
 from engine import search
 from engine import pieces
 from engine import bitboard
 from engine import move
+from engine import evaluate
+from engine import tapered_eval
