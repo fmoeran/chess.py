e52265ae8c68a5b6b2ef36a9691e3071f01e19a9
Felix Moeran <felixmoeran@gmail.com>
2023-08-19 13:38:19 +0100

    Update run_bot

--- engine/run_bot.py
+++ engine/run_bot.py
@@ -1,12 +1,9 @@
-from engine.game import Game
-from engine import search
-
-bot = search.Bot()
-
-
+import engine
 
 if __name__ == "__main__":
-    game = Game(debug=True, black_is_ai=True, white_is_ai=False)
+    engine.game.starting_fen = "8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - - 0 1"
+    bot = engine.search.Bot()
+    game = engine.game.Game(black_is_ai=True, white_is_ai=False, debug=True)
     game.set_ai(bot.search)
-    game.set_ai(bot.search, colour=0)
-    print(game.run(show_fps=True))
+    game.set_ai(bot.search, colour=engine.pieces.black)
+    game.run()
